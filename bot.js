b58b00758a44aff8e44c22fde920a1b2d7a2a520
xHexon <42037258+xHexon@users.noreply.github.com>
2018-08-04 23:17:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "+";
+
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
+
+client.login('Token');
+
+
+
+
+
+
+
 
 var guilds = {};
 client.on('guildBanAdd', function(guild) {
             const rebellog = client.channels.find("name", "log"),
-            Onumber = 10,
+            Onumber = 3,
   Otime = 10000
 guild.fetchAuditLogs({
     type: 22
 }).then(audit => {
     let banner = audit.entries.map(banner => banner.executor.id)
-    let bans = guilds[guild.id + banner].bans || 0
+    let bans = guilds[guild.id + banner].bans || 0 
     guilds[guild.id + banner] = {
         bans: 0
     }
-      bans[guilds.id].bans += 3;
+      bans[guilds.id].bans += 1; 
 if(guilds[guild.id + banner].bans >= Onumber) {
 try {
 let roles = guild.members.get(banner).roles.array();
 guild.members.get(banner).removeRoles(roles);
+  guild.guild.member(banner).kick();
 
 } catch (error) {
 console.log(error)
 try {
-guild.members.get(banner).removeRoles(roles);
+guild.members.get(banner).ban();
   rebellog.send(`<@!${banner.id}>
 حآول العبث بالسيرفر @everyone`);
 guild.owner.send(`<@!${banner.id}>
@@ -52,7 +57,7 @@ console.log(error)
   client.on('channelCreate', async (channel) => {
   const rebellog = client.channels.find("name", "log"),
   Oguild = channel.guild,
-  Onumber = 10,
+  Onumber = 3,
   Otime = 10000;
   const audit = await channel.guild.fetchAuditLogs({limit: 1});
   const channelcreate = audit.entries.first().executor;
@@ -62,10 +67,9 @@ console.log(error)
     created : 0
      }
  }
- channelc[channelcreate.id].created += 3;
+ channelc[channelcreate.id].created += 1;
  if(channelc[channelcreate.id].created >= Onumber ) {
-let roles = guild.members.get(banner).roles.array();
-guild.members.get(banner).removeRoles(roles);
+    Oguild.members.get(channelcreate.id).kick();
 rebellog.send(`<@!${channelcreate.id}>
 حآول العبث بالسيرفر @everyone`);
 channel.guild.owner.send(`<@!${channelcreate.id}>
@@ -80,7 +84,7 @@ let channelr = {};
   client.on('channelDelete', async (channel) => {
   const rebellog = client.channels.find("name", "log"),
   Oguild = channel.guild,
-  Onumber = 10,
+  Onumber = 3,
   Otime = 10000;
   const audit = await channel.guild.fetchAuditLogs({limit: 1});
   const channelremover = audit.entries.first().executor;
@@ -90,10 +94,9 @@ let channelr = {};
     deleted : 0
      }
  }
- channelr[channelremover.id].deleted += 3;
+ channelr[channelremover.id].deleted += 1;
  if(channelr[channelremover.id].deleted >= Onumber ) {
-let roles = guild.members.get(banner).roles.array();
-guild.members.get(banner).removeRoles(roles);
+  Oguild.guild.member(channelremover).kick();
 rebellog.send(`<@!${channelremover.id}>
 حآول العبث بالسيرفر @everyone`);
 channel.guild.owner.send(`<@!${channelremover.id}>
@@ -103,6 +106,14 @@ channel.guild.owner.send(`<@!${channelremover.id}>
  channelr[channelremover.id].deleted = 0;
   },Otime)
   });
+
+client.on('message', msg => {
+	var prefix = "+";
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
 
      client.on('message', message => {
 	   if(message.content.startsWith(`${prefix}invite`)){
@@ -132,7 +143,7 @@ client.on('message', message => {
 
   client.on('ready', function(){
     var ms = 1000 ;
-    var setGame = [`${prefix}invite | BOT By xHexon `,` ${prefix}support | BOT By xHexon `,`دخل البوت سيرفرك و اعطه اعلى رتبة و سوي روم اسمه log و بس:D  `,`${client.users.size} Users | Bot By xHexon `];
+    var setGame = +invite | BOT By xHexon `,` +support | BOT By xHexon `,`دخل البوت سيرفرك و اعطه اعلى رتبة و سوي روم اسمه log و بس:D  `,`${client.users.size} Users | Bot By xHexon `];
     var i = -1;
     var j = 0;
     setInterval(function (){
