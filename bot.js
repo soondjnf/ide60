5206d2a25f2ce8abf0f662c35b4c092a3213cd9b
xHexon <42037258+xHexon@users.noreply.github.com>
2018-08-04 03:49:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,7 @@ client.on("message", message => {
 client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('%bcall')){
+if (message.content.startsWith('+bcall')){
  if (message.author.id !== '385103221804236800') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
  if(!message.author.id === '385103221804236800') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
