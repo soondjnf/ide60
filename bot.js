320e43774d605848aad4787d194e5f951f8d5596
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-26 03:59:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -769,7 +769,6 @@ client.on('guildMemberAdd',async member => {
   
 
 
-const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
@@ -779,15 +778,6 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const client = new Discord.Client();
 
-/*
-البكجآت
-npm install discord.js
-npm install ytdl-core
-npm install get-youtube-id
-npm install youtube-info
-npm install simple-youtube-api
-npm install queue
-*/
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
