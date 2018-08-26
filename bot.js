2e039049f32622572175c9f971fdd30618f1c31b
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-26 04:22:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -776,8 +776,6 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
-const client = new Discord.Client();
-
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
