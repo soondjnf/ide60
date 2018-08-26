e56e1683a53038ef4223a60987d011f8aa0f74ef
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-26 03:29:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -751,7 +751,22 @@ client.on('guildMemberAdd',async member => {
  });//jackeo جاكيو
 
 
-
+  client.on('message', message => {
+   if(message.content.startsWith(prefix + "invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+               let mmmmEmbed = new Discord.RichEmbed()
+                         .setAuthor(client.user.username)
+                         .setThumbnail(message.author.avatarURL)
+ .addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
+           .setFooter(`- Requested By: ${message.author.tag}`);
+           message.channel.send(mmmmEmbed)
+});
+  }
+});
+  
 
 
 
