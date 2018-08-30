9ca5b4d7a3e5f58a18895191e66779c442236c01
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-31 01:58:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1057,43 +1057,45 @@ client.on('raw', event => {
 
 
 client.on('message', message => {
- var prefix = "$ban"
-    if (message.author.Alpha) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-  
-    let args = message.content.split(" ").slice(1);
-
-if (command = prefix) {
-if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-let user = message.mentions.users.first();
-let reason = message.content.split(" ").slice(2).join(" ");
-
-if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-if(!reason) return message.reply ("**اكتب سبب الطرد**");
-if(!reason) reason = "غير محدد";
-if (!message.guild.member(user)
-
-.bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
-message.guild.member(user).ban(7, user);
-
-const banembed = new Discord.RichEmbed()
-.setAuthor(`BANNED!`, user.displayAvatarURL)
-.setColor("RANDOM")
-.setTimestamp()
-.addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-.addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-.addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-message.channel.send({embed : banembed});
-
-member.createDM().then(function (channel) {
-return channel.send(`***${user} تم اعطائك بان من سيرفر ${message.guild.name,true} بسبب ${reason} و الضي اعطاك البان هو ${message.author.tag}`)
+    var prefix = "$ban"
+       if (message.author.Alpha) return;
+       if (!message.content.startsWith(prefix)) return;
+   
+       let command = message.content.split(" ")[0];
+       command = command.slice(prefix.length);
+     
+       let args = message.content.split(" ").slice(1);
+   
+   if (command = prefix) {
+   if(!message.channel.guild) return message.reply('** This command only for servers**');
+   
+   if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+   if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+   let user = message.mentions.users.first();
+   let reason = message.content.split(" ").slice(2).join(" ");
+   
+   if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+   if(!reason) return message.reply ("**اكتب سبب الطرد**");
+   if(!reason) reason = "غير محدد";
+   if (!message.guild.member(user)
+   
+   .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+   
+   message.guild.member(user).ban(7, user);
+   
+   const banembed = new Discord.RichEmbed()
+   .setAuthor(`BANNED!`, user.displayAvatarURL)
+   .setColor("RANDOM")
+   .setTimestamp()
+   .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+   .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+   .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+   message.channel.send({embed : banembed});
+   
+   member.createDM().then(function (channel) {
+   return channel.send(`***${user} تم اعطائك بان من سيرفر ${message.guild.name,true} بسبب ${reason} و الضي اعطاك البان هو ${message.author.tag}`)
+}).catch(console.error)
+})
 });
 
   client.login(process.env.BOT_TOKEN);
