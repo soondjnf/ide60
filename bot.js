b88d276adb920128e7c3bbe949fcd06e060f559e
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-31 01:22:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -531,12 +531,6 @@ message.channel.sendFile(canvas.toBuffer())
 
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const fs = require('fs');
-const moment = require('moment');
-const jimp = require('jimp');
-const Canvas = require('canvas');
 
 client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'welcome');
