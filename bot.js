71e769d89ffbeb720b6a97741c5d3bb368f52f92
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-31 00:01:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1035,5 +1035,31 @@ client.on('raw', event => {
 });
 
 
+client.on('message', message => {
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+  
+    let args = message.content.split(" ").slice(1);
+if (message.content === '$ban') {
+if(!message.channel.guild) return message.reply('** This command only for servers**');
+if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+let user = message.mentions.users.first();
+let reason = message.content.split(" ").slice(2).join(" ");
+if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+if(!reason) return message.reply ("**اكتب سبب الطرد**");
+if (!message.guild.member(user)
+.bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+message.guild.member(user).ban(7, user);
+let banembed = new Discord.RichEmbed()
+.setAuthor(`BANNED!`, user.displayAvatarURL)
+.setColor("RANDOM")
+.setTimestamp()
+.addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+.addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+.addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+message.channel.send({embed : banembed});
+}
+});
 
   client.login(process.env.BOT_TOKEN);
